a74d2bf7325191f09ff5d79bc3349a1c929560bb
jeffreyyzwu <154338696@qq.com>
2014-03-27 22:48:49 +0800

    install mongoDB

--- app.js
+++ app.js
@@ -33,5 +33,5 @@ app.get('/users', user.list);
 app.post('/sqlinfo', routes.getsqlinfo)
 
 http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+  console.log('Blackhole server listening on port ' + app.get('port'));
 });
